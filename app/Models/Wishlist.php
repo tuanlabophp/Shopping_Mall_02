f27703496efdf87f11ca23a9508a50eef2881034
tuanlabophp <tuan.nguyenducanh@gmail.com>
2017-05-09 10:15:52 +0700

    add bower

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -16,7 +16,7 @@ class Wishlist extends Model
     {
         return $this->belongsTo(User::class);
     }
-    public function users()
+    public function products()
     {
         return $this->belongsTo(Product::class);
     }
